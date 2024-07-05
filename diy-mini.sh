090c63cbfa063d5801b7ed47a975eb50274f12e8
banban2025 <bandomkings538@gmail.com>
2024-07-05 20:40:59 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -33,6 +33,9 @@ function git_sparse_clone() {
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # 科学上网插件
+rm -rf package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/haiibo/openwrt-packages package/openwrt-packages
 #git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
